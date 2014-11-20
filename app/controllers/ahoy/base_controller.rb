5fb0e6d0a80f1b88370cd43418f1463e5e65b185
Edwin Wills <edwin@rentify.com>
2014-11-20 18:54:16 +0000

    Added the ability to not skip named controller filters

--- app/controllers/ahoy/base_controller.rb
+++ app/controllers/ahoy/base_controller.rb
@@ -1,10 +1,20 @@
 module Ahoy
   class BaseController < ApplicationController
     # skip all filters
-    skip_filter *_process_action_callbacks.map(&:filter)
+    skip_filter :skip_filters
 
     def ahoy
       @ahoy ||= Ahoy::Tracker.new(controller: self, api: true)
+    end
+
+    private
+
+    def exclude_filters
+      []
+    end
+
+    def skip_filters
+      BaseController._process_action_callbacks.map(&:filter).reject{ |filter| exclude_filters.include?(filter) }
     end
 
   end
